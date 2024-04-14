1d121c7ef2fac1732b232079472a3e749d3a35f3
Bahadir UNGOR <bahadirungor@gmail.com>
2024-04-15 00:58:50 +0300

    BST structure created

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -1,14 +1,25 @@
 function BST(){
-  function Node(){
-    return { key, left, right }
+  function _Node(data, left=null, right=null){
+    return { data, left, right }
   }
   
-  function Tree(arr){
-    let root 
+  function tree(arr){
+    let sorted = [...new Set(arr)].sort()
+    let root = _buildTree(sorted, 0, sorted.length-1)
+    return root
   }
 
-  function buildTree(arr){
+  function _buildTree(arr, start, end){
+    if(start>end) return null;
 
+    let mid = Math.floor((start+end)/2);
+
+    const node = _Node(arr[mid]);
+
+    node.left = _buildTree(arr, start, mid - 1);
+    node.right = _buildTree(arr, mid + 1, end);
+
+    return node
   }
 
   const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -24,5 +35,9 @@ function BST(){
     }
   };
 
-  return { buildTree, prettyPrint}
+  return { tree, prettyPrint}
 }
+
+let deneme = BST()
+let rot = deneme.tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+console.log(deneme.prettyPrint(rot))
