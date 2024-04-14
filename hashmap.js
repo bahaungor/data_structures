a021a17c13ad0e731dda6399e4d892f89598b0ed
Bahadir UNGOR <bahadirungor@gmail.com>
2024-04-14 04:45:59 +0300

    set and resize imp to hashmap

--- hashmap.js
+++ hashmap.js
@@ -1,9 +1,9 @@
 function hashMap(){
-  let bucketSize = 16
-  let loadFactor = 0.99
-  let size = 0
+  let bucketSize = 8;
+  let loadFactor = 0.7;
+  let size = 0;
   
-  let bucket = new Array(bucketSize)
+  let bucket = Array(bucketSize).fill(null).map(() => []);
 
   function hash(key) {
       let hashCode = 0;
@@ -11,29 +11,53 @@ function hashMap(){
       const primeNumber = 31;
       for (let i = 0; i < key.length; i++) {
         hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        hashCode %= bucketSize
+        hashCode %= bucketSize;
       }
     
       return hashCode;
   }
 
   function resize(){
-    
+    bucketSize *= 2;
+    let newBucket = new Array(bucketSize).fill(null).map(() => []);
+    console.log(size)
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].length > 0){
+        let currentNode = bucket[i][0];
+        
+        while(currentNode !== null){
+          const hashCode = hash(currentNode.key)
+          if (newBucket[hashCode].length === 0){
+            newBucket[hashCode].push({ key:currentNode.key, value:currentNode.value, next:null })
+          } else {
+            currentNode.next = { key:currentNode.key, value:currentNode.value, next:null }
+          }
+          currentNode = currentNode.next;
+        }
+        
+      }
+    }
+
+    return newBucket
   }
 
   function set(key, value) {
-    if (size/bucketSize > loadFactor) resize()
+    console.log("set called")
+    if (size/bucketSize > loadFactor) {
+      bucket = resize()
+    }
     const hashCode = hash(key)
 
     if (!bucket[hashCode] || !bucket[hashCode].length){
-      bucket[hashCode].push({ key, value, next:null })
-      size++
+      bucket[hashCode].push({ key, value, next:null });
+      size++;
     } else {
         let currentNode = bucket[hashCode][0];
 
         while (currentNode.next !== null || currentNode.key === key){
           if (currentNode.key === key){
-            currentNode.value = value
+            currentNode.value = value;
+            return;
           } else {
             currentNode = currentNode.next
           }
@@ -43,10 +67,18 @@ function hashMap(){
         size++
     }
   
-
   }
-  return {set}
+  return { set }
 }
 
 deneme = hashMap()
-console.log(deneme.hash("Bahadır"))
+let we = deneme.set("Bahadır", "Ungor")
+we =deneme.set("asd", "qwe")
+we =deneme.set("wererg", "ntyrtr")
+we =deneme.set("vsfddw", "wervxc")
+we =deneme.set("123123", "123123")
+we =deneme.set("qewqe112", "12312dasda")
+we =deneme.set("sdasqw", "vsvs32")
+we =deneme.set("vsdfsfd", "adasdwww")
+
+console.log(we)
