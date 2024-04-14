1a9c1c8503558af46843ce10b5b23f8e972c14b0
Bahadir UNGOR <bahadirungor@gmail.com>
2024-04-14 20:11:40 +0300

    bst created

---
+++ binary_search_tree.js
@@ -0,0 +1,28 @@
+function BST(){
+  function Node(){
+    return { key, left, right }
+  }
+  
+  function Tree(arr){
+    let root 
+  }
+
+  function buildTree(arr){
+
+  }
+
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+  return { buildTree, prettyPrint}
+}
